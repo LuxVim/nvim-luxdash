f36d4415f6f2defb254d29df696f13d63b0a5686
josstei <joseph.steinbrenner@yahoo.com>
2025-07-31 22:56:04 -0400

    bug fixes for menu defaults

--- lua/luxdash/menu/closelux.lua
+++ lua/luxdash/menu/closelux.lua
@@ -5,7 +5,7 @@ function M.command()
     keymap = "q",
     label = "Close LuxDash",
     command = function()
-      local float = require('luxdash.float')
+      local float = require('luxdash.ui.float_manager')
       if float.is_open() then
         float.close()
       else
--- lua/luxdash/menu/newfile.lua
+++ lua/luxdash/menu/newfile.lua
@@ -5,7 +5,7 @@ function M.command()
     keymap = "n",
     label = "New File",
     command = function()
-      local float = require('luxdash.float')
+      local float = require('luxdash.ui.float_manager')
       if float.is_open() then
         float.close()
       end
